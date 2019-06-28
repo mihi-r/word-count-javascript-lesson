027bbfc6a727ed5107612f8d375ebeb2da0e5cd5
patel3mh <patel3mh@mail.uc.edu>
2019-06-27 22:43:05 -0400

    Added the secondary functionality

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ const intialInput = document.querySelector('.initial-input');
 const wordCount = document.querySelector('.word-count');
 const countButton = document.querySelector('.count-btn');
 const parsedInput = document.querySelector('.parsed-input');
+const specificWord = document.querySelector('.specific-word');
 
 countButton.onclick = function countButtonOnClicked() {
-    
   while (parsedInput.children.length > 1) {
     parsedInput.lastChild.remove();
   }
@@ -14,7 +14,11 @@ countButton.onclick = function countButtonOnClicked() {
   let preprocessedInput = intialInput.value.trim().toLowerCase().replace(/(\.|,|!|\?|\(|\)|'|")/gm, '').split(' ');
   preprocessedInput = preprocessedInput.filter(word => word !== '');
 
-  parseInput(preprocessedInput, parsedInput);
+  parseInput(preprocessedInput, parsedInput, specificWord.value.toLowerCase());
+
+  if (specificWord.value) {
+    preprocessedInput = preprocessedInput.filter(word => word === specificWord.value.toLowerCase());
+  }
 
   wordCount.textContent = `Word count: ${preprocessedInput.length}`;
 };
--- scripts/parser.js
+++ scripts/parser.js
@@ -1,9 +1,13 @@
-export default function parseInput(input, parseContainer) {
+export default function parseInput(input, parseContainer, wordToHighlight = '') {
   const ulNode = document.createElement('ul');
 
   input.forEach((word) => {
     const liNode = document.createElement('li');
     liNode.textContent = word;
+
+    if (wordToHighlight === word) {
+      liNode.classList.add('word-found');
+    }
     ulNode.appendChild(liNode);
   });
 
